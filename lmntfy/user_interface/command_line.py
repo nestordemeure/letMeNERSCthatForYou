f423023b5b36d746562e24752ea274c2277078a6
nestor <ndemeure@lbl.gov>
2023-06-27 11:54:10 -0700

    improved command line formating

--- lmntfy/user_interface/command_line.py
+++ lmntfy/user_interface/command_line.py
@@ -29,28 +29,34 @@ def answer_questions(question_answerer:QuestionAnswerer, questions:List[str]) ->
     """run on a handful of test question for quick evaluation purposes"""
     answers = []
     console = Console()
+    print()
     for question in questions:
         # displays question
-        print(f"\n> {question}\n")
+        print(f"> {question}")
         # gets an answer and stores it
         answer = question_answerer.get_answer(question, verbose=False)
         answers.append(answer)
         # pretty prints the answer
-        markdown_answer = Markdown(f"\n{answer}\n")
+        markdown_answer = Markdown(answer)
+        print()
         console.print(markdown_answer)
+        print()
     return answers
 
 def chat(question_answerer:QuestionAnswerer) -> List[Dict]:
     """chat with the model"""
     messages = []
     console = Console()
+    print()
     while True:
         # gets question
-        question = input("\n> ")
+        question = input("> ")
         messages.append({'role':'user', 'content': question})
         # gets an answer and stores it
         answer_message = question_answerer.continue_chat(messages, verbose=False)
         messages.append(answer_message)
         # pretty prints the answer
-        markdown_answer = Markdown(f"\n\n{answer_message['content']}\n")
+        markdown_answer = Markdown(answer_message['content'])
+        print()
         console.print(markdown_answer)
+        print()
