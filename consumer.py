dd9a3ca138bc8c857adcb2ee41d2bfc0ac81b785
nestor <ndemeure@lbl.gov>
2023-07-06 11:06:30 -0700

    aded markdown rendering to the api

--- consumer.py
+++ consumer.py
@@ -3,6 +3,7 @@ import time
 import requests
 import lmntfy
 import argparse
+import markdown
 from pathlib import Path
 
 def parse_args():
@@ -50,6 +51,8 @@ def main():
             # gets an answer from the model
             try:
                 answer = question_answerer.continue_chat(messages, verbose=False)
+                # renders the markdown as html
+                answer['content'] = markdown.markdown(answer['content'])
             except Exception as e:
                 answer = {'role':'assistant', 'content': f"ERROR: {str(e)}"}
             # post the answer with the conversation key
