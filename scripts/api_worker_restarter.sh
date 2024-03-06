6de6f8ed2e6f7958b3936dab59b12c3aa61267fb
nestor <ndemeure@lbl.gov>
2024-03-06 14:35:10 -0800

    updated restarter script

--- scripts/api_worker_restarter.sh
+++ scripts/api_worker_restarter.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Purpose: Restart a specific worker script if it's not currently running and no restart job is pending or running,
-# taking into account system maintenance schedules and ensuring output is logged appropriately.
 
 # Configuration variables
-BUFFER_BEFORE_MAINTENANCE_MIN=30 # Buffer time in minutes before system maintenance begins
 JOB_NAME=singleton_chatbot_worker # Name of the original job to check in the SLURM queue
 RESTART_JOB_NAME="${JOB_NAME}_restarted" # Name for restarted job
 RESTART_JOB_INDICATOR="restart" # Indicator to identify restart jobs, can be part of the job name or description
@@ -12,48 +10,34 @@ OUTPUT_PATH="/global/cfs/cdirs/nstaff/chatbot/letMeNERSCthatForYou/data/logs/api
 
 # Check if the original job is already running
 ORIGINAL_JOB_CHECK=$(squeue --noheader -n $JOB_NAME --state=running -u $USER -o "%12i %2t %9u %25j %6D %10M %12q %8f %18R")
-IS_ORIGINAL_JOB_RUNNING=$(echo "$ORIGINAL_JOB_CHECK" | wc -l)
+IS_ORIGINAL_JOB_RUNNING=$(echo "$ORIGINAL_JOB_CHECK" | grep -c '.')
 
 # If the original job is running, log this and exit
 if [[ "$IS_ORIGINAL_JOB_RUNNING" -ge 1 ]]; then
-    printf "[%s] Original job %s is already running\n" $(date "+%m-%d-%Y-%H:%M:%S") "$JOB_NAME"
-    printf "===============================\n%s\n===============================\n" "$ORIGINAL_JOB_CHECK"
+    printf "[%s] Original job %s is already running:\n" $(date "+%m-%d-%Y-%H:%M:%S") "$JOB_NAME"
+    printf "%s\n" "$ORIGINAL_JOB_CHECK"
+    printf "===============================\n"
     exit 0;
+else
+    printf "[%s] NO original job %s is running:\n" $(date "+%m-%d-%Y-%H:%M:%S") "$JOB_NAME"
+    printf "> check:'%s' test:'%s'\n" "$ORIGINAL_JOB_CHECK" "$IS_ORIGINAL_JOB_RUNNING"
+    printf "===============================\n"
 fi
 
 # Check if the restart job is already running or pending
 RESTART_JOB_CHECK=$(squeue --noheader -n $RESTART_JOB_NAME --state=running,pending -u $USER -o "%12i %2t %9u %25j %6D %10M %12q %8f %18R")
-IS_RESTART_JOB_RUNNING_OR_PENDING=$(echo "$RESTART_JOB_CHECK" | wc -l)
+IS_RESTART_JOB_RUNNING_OR_PENDING=$(echo "$RESTART_JOB_CHECK" | grep -c '.')
 
 # If a restart job is running or already in the queue, log this and exit
 if [[ "$IS_RESTART_JOB_RUNNING_OR_PENDING" -ge 1 ]]; then
-    printf "[%s] Restart job %s is already running or in the queue\n" $(date "+%m-%d-%Y-%H:%M:%S") "$RESTART_JOB_NAME"
-    printf "===============================\n%s\n===============================\n" "$RESTART_JOB_CHECK"
+    printf "[%s] Restart job %s is already running or in the queue:\n" $(date "+%m-%d-%Y-%H:%M:%S") "$RESTART_JOB_NAME"
+    printf "%s\n" "$RESTART_JOB_CHECK"
+    printf "===============================\n"
     exit 0;
-fi
-
-# Check for scheduled system maintenance
-MAINTENANCE_TIME=$(scontrol show res -o | egrep 'login|workflow' | awk -F'[= ]' '{print $4}' | head -n 1)
-
-# If no maintenance is scheduled, proceed without adjusting for maintenance
-if [[ "$MAINTENANCE_TIME" == "" ]]; then
-    printf '[%s] No maintenance found\n' $(date "+%m-%d-%Y-%H:%M:%S")
-    TIME_MIN_STR=""
 else
-    # Calculate time until maintenance starts
-    MAINTENANCE_START=$(date "+%s" -d $MAINTENANCE_TIME)
-    CURRENT_TIME=$(date "+%s")
-    TIME_UNTIL_MAINTENANCE_MIN=$(((MAINTENANCE_START-CURRENT_TIME)/60))
-
-    printf '[%s] Minutes before next maintenance: %s\n' $(date "+%m-%d-%Y-%H:%M:%S") "$TIME_UNTIL_MAINTENANCE_MIN"
-
-    # Adjust for buffer before maintenance
-    TIME_MIN=$((TIME_UNTIL_MAINTENANCE_MIN-BUFFER_BEFORE_MAINTENANCE_MIN))
-    if [[ "$TIME_MIN" -le 0 ]]; then
-        TIME_MIN_STR=""
-    else
-        TIME_MIN_STR="--time-min=${TIME_MIN}"
-    fi
+    printf "[%s] NO restart job %s is running or pending:\n" $(date "+%m-%d-%Y-%H:%M:%S") "$RESTART_JOB_NAME"
+    printf "> check:'%s' test:'%s'\n" "$RESTART_JOB_CHECK" "$IS_RESTART_JOB_RUNNING_OR_PENDING"
+    printf "===============================\n"
 fi
 
 # Calculate the duration until the target end time
@@ -62,7 +46,7 @@ DURATION=$((END_TIME - CURRENT_TIME))
 DURATION_HMS=$(printf '%02d:%02d:%02d' $(($DURATION / 3600)) $(($DURATION % 3600 / 60)) $(($DURATION % 60)))
 
 # Update the SBATCH command to include the new job name for restarts
-SBATCH_COMMAND="sbatch --dependency=singleton ${TIME_MIN_STR} --time=$DURATION_HMS --qos shared --output=$OUTPUT_PATH -J $RESTART_JOB_NAME /global/cfs/cdirs/nstaff/chatbot/letMeNERSCthatForYou/scripts/api_worker.sh"
+SBATCH_COMMAND="sbatch --dependency=singleton --time=$DURATION_HMS --qos cron --output=$OUTPUT_PATH -J $RESTART_JOB_NAME /global/cfs/cdirs/nstaff/chatbot/letMeNERSCthatForYou/scripts/api_worker.sh"
 
 # Echo the sbatch command for logging
 printf '[%s] %s \n' $(date "+%m-%d-%Y-%H:%M:%S") "$SBATCH_COMMAND"
@@ -72,3 +56,4 @@ unset ${!SLURM_@};
 
 # Submit the job with sbatch
 eval $SBATCH_COMMAND
+printf "===============================\n"
