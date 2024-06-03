f54bfa72bee70ffe82e539b78eb47ed34faeba74
nestor <ndemeure@lbl.gov>
2024-06-03 15:54:57 -0700

    move scripts to shifter

--- scripts/api_chatbot.sh
+++ scripts/api_chatbot.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# load dependencies
-module load python/3.10
-conda activate /global/cfs/cdirs/nstaff/chatbot/conda/chatbot
-
 # folders
 chatbot_root="/global/cfs/cdirs/nstaff/chatbot"
 code_folder="$chatbot_root/production_code"
-python_instance="$chatbot_root/conda/chatbot/bin/python3"
+python_instance="shifter --module=gpu,nccl-2.18 --image=asnaylor/lmntfy:v0.3 python3"
 
 # runs the client
 # Using python_instance to run the api_client script in code_folder
--- scripts/dev_local_chatbot.sh
+++ scripts/dev_local_chatbot.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# load dependencies
-module load python/3.10 cudatoolkit/12.0 cudnn/8.9.3_cuda12 pytorch/2.1.0-cu12
-conda activate /global/cfs/cdirs/nstaff/chatbot/conda/chatbot
-
 # base folder
 chatbot_root="/global/cfs/cdirs/nstaff/chatbot"
 # local folders
@@ -16,7 +12,7 @@ data_folder="$code_folder/data"
 # data folders
 documentation_folder="$data_folder/nersc_doc/docs"
 database_folder="$data_folder/database"
-python_instance="$chatbot_root/conda/chatbot/bin/python3"
+python_instance="shifter --module=gpu,nccl-2.18 --image=asnaylor/lmntfy:v0.3 python3"
 
 # runs the worker
 # Using python_instance to run the chatbot script in dev_code_folder
